2fd1544707bb9183329121d06a85dd3d43a069a2
Dipratna Mahire <dipratnamahire2222@gmail.com>
2026-02-05 17:52:07 +0530

    ok

--- src/Components/BbaAdmission/BbaAdmissionPointers.jsx
+++ src/Components/BbaAdmission/BbaAdmissionPointers.jsx
@@ -54,13 +54,13 @@ function MarketingManagementPointers() {
                        transition-all duration-300 ease-in-out
                        hover:scale-105 hover:shadow-2xl hover:bg-[#E9F7FA]"
           >
-            <Icon size={30} className="text-[#023B5E]" />
+            <Icon size={30} className="text-[#790D20]" />
 
-            <h2 className="text-[#135783] text-xl md:text-xl font-semibold mt-2 text-center">
+            <h2 className=" text-xl md:text-xl font-semibold mt-2 text-center">
               {title}
             </h2>
 
-            <p className="text-[#135783] text-xl md:text-xl text-center mt-1">
+            <p className=" text-xl md:text-xl text-center mt-1">
               {value}
             </p>
           </div>
