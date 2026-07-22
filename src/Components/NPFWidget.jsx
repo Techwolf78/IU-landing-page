32adea9197c7c55471e3bfe35bce7e29be48ac6e
Techwolf78 <mrajaypawar207@gmail.com>
2026-07-22 11:13:59 +0530

    Set NPFWidget redirect URL to thankyou page

--- src/Components/NPFWidget.jsx
+++ src/Components/NPFWidget.jsx
@@ -28,11 +28,14 @@ const NPFWidget = () => {
     };
   }, []);
 
+  const redirectUrl = encodeURIComponent("https://bba-sob.indirauniversity.edu.in/thankyou");
+
   return (
     <div
       className="npf_wgts"
       data-height={height}
       data-w="485adad5d85a233b731dd5cc763e58ba"
+      data-r={redirectUrl}
       style={{ minHeight: height }}
     ></div>
   );
